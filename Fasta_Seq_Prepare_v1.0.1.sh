9cef67f71f07eff45e167e54899bff90cb46521c
rodolfo.aramayo.org <rodolfo@aramayo.org>
2024-04-02 18:22:55 -0500

    Adding DOI corresponding to the Zenodo publication Fixing minor log-related typos

--- Fasta_Seq_Prepare_v1.0.1.sh
+++ Fasta_Seq_Prepare_v1.0.1.sh
@@ -138,13 +138,13 @@ EOF
 };
 
 ## Defining_Script_Current_Version
-version="1.0.0";
+version="1.0.1";
 
 ## Defining_Script_Initial_Version_Data (date '+DATE:%Y/%m/%d%tTIME:%R')
 version_date_initial="DATE:2020/08/17   TIME:00:00";
 
 ## Defining_Script_Current_Version_Data (date '+DATE:%Y/%m/%d%tTIME:%R')
-version_date_current="DATE:2024/04/01	TIME:13:15";
+version_date_current="date:2024/04/02	TIME:17:51";
 
 ## Testing_Script_Input
 ## Is the number of arguments null?
@@ -486,6 +486,11 @@ else
     rm ./"${INFILE01%.fa}"_Fasta_Seq_Prepare.dir/* &>/dev/null;
 fi
 
+if [[ -d ./${INFILE01%.fa}_Fasta_Seq_Prepare.tmp ]];
+then
+    rm -fr ./${INFILE01%.fa}_Fasta_Seq_Prepare.tmp;
+fi
+
 ## Generating/Cleaning TMP Data Directory
 if [[ $var_tmp_dir -eq 0 ]];
 then
@@ -495,14 +500,14 @@ then
     fi
     if [[ -z $TMPDIR ]];
     then
-        ## echo TMPDIR not defined
+        ## echo "TMPDIR not defined";
         TMP=$(mktemp -d -p "$TMP"); ## &> /dev/null);
         var_script_tmp_data_dir="$TMP";
         export  var_script_tmp_data_dir="$TMP";
     fi
     if [[ ! -z $TMPDIR ]];
     then
-        ## echo TMPDIR defined;
+        ## echo "TMPDIR defined";
         TMP=$(mktemp -d -p "$TMPDIR"); ## &> /dev/null);
         var_script_tmp_data_dir="$TMP";
         export  var_script_tmp_data_dir="$TMP";
@@ -513,8 +518,8 @@ fi
 if [[ $var_tmp_dir -eq 1 ]];
 then
     ## Defining Script TMP Data Directory
-    var_script_tmp_data_dir="$(pwd)/"${INFILE01%.fa}".tmp";
-    export var_script_tmp_data_dir="$(pwd)/"${INFILE01%.fa}".tmp";
+    var_script_tmp_data_dir="$(pwd)/"${INFILE01%.fa}"_Fasta_Seq_Prepare.tmp";
+    export var_script_tmp_data_dir="$(pwd)/"${INFILE01%.fa}"_Fasta_Seq_Prepare.tmp";
 
     if [[ ! -d $(basename "$var_script_tmp_data_dir") ]];
     then
@@ -700,9 +705,9 @@ fi
 ## Checking where the TMPDIR will be generated
 if [[ $var_tmp_dir -eq 0 ]];
 then
-    echo -e "\t-z Flag: Normal Run for the TMPDIR was Requested" >> ./"${INFILE01%.fa}"_Fasta_Seq_Prepare.dir/"${INFILE01%.fa}"_Fasta_Seq_Prep.log;
-else
     echo -e "\t-z Flag: TMPDIR Run for the TMPDIR was Requested" >> ./"${INFILE01%.fa}"_Fasta_Seq_Prepare.dir/"${INFILE01%.fa}"_Fasta_Seq_Prep.log;
+else
+    echo -e "\t-z Flag: Normal Run for the TMPDIR was Requested" >> ./"${INFILE01%.fa}"_Fasta_Seq_Prepare.dir/"${INFILE01%.fa}"_Fasta_Seq_Prep.log;
 fi
 
 ##  Remove empty lines (if present)
